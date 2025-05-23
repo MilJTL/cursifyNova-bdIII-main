8a3f238fdbe2aa3c5eb5ee6f149e333caa416d00
MilJTL <miltontoledolopez@gmail.com>
2025-05-23 14:53:12 -0400

    Update app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -18,7 +18,7 @@ app.use(helmet());
 
 // Configuración de CORS
 app.use(cors({
-    //origin: config.clientUrl || 'http://localhost:3000',
+    origin: 'https://cursify-nova-bd-iii-main-pnfc4m9u0-miltons-projects-6aa104f3.vercel.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
