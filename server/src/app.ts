2ce1ab3f6786ed890a04a0e3fed72e8d464770bc
MilJTL <miltontoledolopez@gmail.com>
2025-05-24 19:16:21 -0400

    Update app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -33,7 +33,7 @@ app.use('/api',cors({
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
-}));
+}),routes);
 
 // Logging
 if (config.nodeEnv !== 'test') {
