3e9f9b836152fed045adf64858e8caca0027b203
MilJTL <miltontoledolopez@gmail.com>
2025-05-24 07:43:48 -0400

    Update app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -17,23 +17,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 
 //origenes permitidos
-const allowedOrigins = [
+/*const allowedOrigins = [
   'https://cursify-nova-bd-iii-main.vercel.app',
   'https://cursify-nova-bd-iii-main-git-main-miltons-projects-6aa104f3.vercel.app',
   'https://cursify-nova-bd-iii-main-bkzqk1boq-miltons-projects-6aa104f3.vercel.app',
   // ... otros orígenes permitidos ...
 ];
-
+*/
 // Configuración de CORS
 app.use(cors({
-    origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-}));
+    origin: '*',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
